67b01ee54c38bf902064b3240b405bcdef8cb305
José Anderson Ramos Aquino <34531721+joseandersonramos@users.noreply.github.com>
2022-12-16 17:28:04 -0400

    Modificar texto de salida por pantalla

--- Introducción a la programación/Tema 4/src/com/company/Main.java
+++ Introducción a la programación/Tema 4/src/com/company/Main.java
@@ -1,58 +1,90 @@
 package com.company;
 
+//Ejercicios Tema 4 - Estructuras de control
+
 public class Main {
+    public static void main(String[] args){
 
-    public static void main(String[] args) {
-        // if, else if y else
-        int numeroIf = 0;
+        /*
+        1-Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+        Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
+        */
 
-        if (numeroIf<0){
-            System.out.println("La variable numeroIf " + numeroIf + " es negativo");
+        int numeroIf = 1;
+
+        if(numeroIf > 0){
+            System.out.println("El número " + numeroIf + " es positivo.");
         }
-        else if(numeroIf>0){
-            System.out.println("La variable numeroIf " + numeroIf + " es positivo");
-        } else {
-            System.out.println("La variable numeroIf es 0");
+
+        else if(numeroIf < 0){
+            System.out.println("El número " + numeroIf + " es negativo.");
         }
 
-        //while
-        int numeroWhile = 1;
+        else {
+            System.out.println("El número " + numeroIf + " es 0.");
+        }
 
-        while(numeroWhile < 3){
+        /*
+        2-Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile
+         sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+            -Incrementar el valor de la variable en uno cada vez que se ejecute.
+            -Mostrarlo por pantalla cada vez que se ejecute.
+         */
+
+        int numeroWhile = 0;
+
+        while (numeroWhile <3){
             numeroWhile++;
             System.out.println("La variable numeroWhile ahora vale: " + numeroWhile);
         }
 
-        //do while
-        //Al no cumplirse la condición, solo entra en el bucle una vez
-        int numeroDoWhile = 3;
-        do{
-            numeroDoWhile++;
-            System.out.println("La variable numeroDoWhile ahora vale: " + numeroDoWhile);
-        }while(numeroDoWhile < 3);
+        /*
+        3-Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se
+         debe ejecutar una vez.
+        */
 
-        //for
-        for(int numeroFor = 0; numeroFor <= 5; numeroFor++){
+        int numeroDoWhile = 1;
+
+        do {
+            System.out.println("La variable numeroDoWhile ahora vale: " + numeroDoWhile);
+            numeroDoWhile++;
+        } while(numeroDoWhile < 0);
+
+        /*
+        4-Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición
+        será que la variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute
+        y deberá mostrarse por pantalla.
+        */
+
+        for (int numeroFor = 0; numeroFor <=3; numeroFor++){
             System.out.println("La variable numeroFor ahora vale: " + numeroFor);
         }
 
-        //switch
-        String estacion = "otoño";
-        switch(estacion) {
-            case "verano":
-                System.out.println("Estamos en verano");
-                break;
-            case "invierno":
-                System.out.println("Estamos en invierno");
-                break;
+        /*
+        5-Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro
+        estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola
+        informando de la estación en la que está. También habrá que poner un default para cuando el valor de la
+        variable no sea una estación.
+        */
+
+        //primavera, verano, otoño e invierno.
+        String estacion = "primavera";
+
+        switch (estacion){
             case "primavera":
-                System.out.println("Estamos en primavera");
+                System.out.println("Estamos en primavera!");
+                break;
+            case "verano":
+                System.out.println("Estamos en verano!");
                 break;
             case "otoño":
-                System.out.println("Estamos en otoño");
+                System.out.println("Estamos en otoño!");
+                break;
+            case "invierno":
+                System.out.println("Estamos en invierno!");
                 break;
             default:
-                System.out.println("No es una estación");
+                System.out.println("Esta no es una estación!");
         }
     }
 }
